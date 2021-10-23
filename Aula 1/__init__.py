a03438c85b84ccecef3733d1ad18c28f77a3e79e
einstein808 <gabryel808einstein@gmail.com>
2021-10-23 12:49:48 -0300

    definindo os atributos.

--- Aula 1/__init__.py
+++ Aula 1/__init__.py
@@ -1,1 +1,30 @@
-print("ola munsdo")
+class funcionario:
+    def setNome(self, nome):
+        self._nome = nome
+
+    def getNome(self):
+        return self._nome
+
+    def setSalBruto(self, salarioBruto):
+        self._salarioBruto = salarioBruto
+
+    def getSalBruto(self):
+        return self._salarioBruto
+
+    def setTotalAcrescimo(self, totalAcrescimo):
+        self._totalAcrescimo = totalAcrescimo
+
+    def getTotalAcrescimo(self):
+        return self._totalAcrescimo
+
+    def setTotalDesconto(self, totalDesconto):
+        self._totalDesconto = totalDesconto
+
+    def getTotalDesconto(self):
+        return self._totalDesconto
+
+    def salarioLiquido(self):
+      return self._salarioBruto + self._totalAcrescimo - self._totalDesconto
+
+funcionario()
+
