2eddf004511553afd8dc2083e90b417f72c01b72
einstein808 <gabryel808einstein@gmail.com>
2021-10-24 00:03:20 -0300

    Update Exerício1.py

---
+++ Aula 1/tópico 2/Exerício1.py
@@ -0,0 +1,26 @@
+class Funcionario:
+    def __init__(self):
+        self._nome = ""
+        self._cpf = ""
+        self._email = ""
+        self._telefone = ""
+        self._salário = ""
+
+class Cliente:
+    def __init__(self):
+        self._nome = ""
+        self._cpf = ""
+        self._email = ""
+        self._telefone = ""
+        self._limiteCredito = ""
+
+class Fornecedor:
+    def __init__(self):
+        self._nome = ""
+        self._cnpj = ""
+        self._email = ""
+        self._telefone = ""
+        self._prazoDeEntrega = ""
+
+
+
