66e992686023a3057844cf105d3b7a6dae0ea40f
einstein808 <gabryel808einstein@gmail.com>
2021-10-24 22:36:37 -0300

    trabalhando nos exercícios.

--- Aula 1/Asociaçao/Exerício-de-associação.py
+++ Aula 1/Asociaçao/Exerício-de-associação.py
@@ -31,8 +31,78 @@ class Cidade:
         else:
             return self.estado.getNome()
 
+class Escolaridade:
+    def __init__(self, descricao):
+        self._descricao = descricao
 
-estado = Estado("rj")
-cidade = Cidade("juiz de fora")
-cidade.setEstado(estado)
-print(cidade.getNomeEstado())
+    def getDescricao(self):
+        return self._descricao
+    def setDescricao(self, descricao):
+        self._descricao = descricao
+
+class Pessoa:
+    def __init__(self, nome):
+        self._nome = nome
+        self._escolaridade = None
+        self._naturalidade = None
+
+    def setEscolaridade(self, escolaridade):
+        self._escolaridade = escolaridade
+    def getEscolaridade(self):
+        return self._escolaridade
+
+    def setNaturalidade(self, cidade):
+        self._naturalidade = cidade
+    def getNaturalidade(self):
+        return self._naturalidade
+
+    def getNome(self):
+        return self._nome
+
+    def setNome(self, nome):
+        self._nome = nome
+    def getDescricaoEscolaridade(self):
+        if self._escolaridade == None:
+           return "sem escolaridade"
+        else:
+            return self._escolaridade.getDescricao()
+
+class Professor(Pessoa):
+    def __init__(self, nome):
+        Pessoa.__init__(self,nome)
+
+class Curso:
+    def __init__(self,nome):
+        self._nome = nome
+        self._coordenador = None
+
+    def getNome(self):
+            return self._nome
+
+    def seNome(self, nome):
+            self._nome = nome
+
+    def getCoordenador(self):
+        return self._coordenador
+    def setCoordenador(self, coordenador):
+        self._coordenador = coordenador
+
+
+
+#estado = Estado("rj")
+# cidade = Cidade("juiz de fora")
+# cidade.setEstado(estado)
+# print(cidade.getNomeEstado())
+
+# escolaridade = Escolaridade("mestradoss")
+# professor = Professor("ana")
+# professor.setEscolaridade(escolaridade)
+# print(professor.getDescricaoEscolaridade())
+
+escolaridade = Escolaridade("Pos-gradução")
+professor = Professor("maria")
+curso = Curso("computaçao")
+professor.setEscolaridade(escolaridade)
+curso.setCoordenador(professor)
+
+print(curso.professor)
