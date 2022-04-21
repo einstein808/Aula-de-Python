7ba87272051f8f50a33d8adba36345e37680e625
Gabryel Einstein de Carvalho Amaro <gabryel808einstein@gmail.com>
2022-04-20 23:18:42 -0300

    araay for

---
+++ exerícios entrega/list no repeat.py
@@ -0,0 +1,4 @@
+list1 = [1,2,3,4,5,6]
+list2 =[1,2,3,8,9,10]
+list1to2=[]
+for y in list1:
---
+++ exerícios entrega/lista em lista.py
@@ -0,0 +1,8 @@
+list1=[1,2,3,4,5,6,7]
+list2 =[1,2,34,5,7]
+list1t2=[]
+for x in list1:
+    list1t2.append(x)
+for y in list2:
+    list1t2.append(y)
+print(list1t2)
