09e096615ec006d890f64281a4a02f0181acfce7
Gabryel Einstein de Carvalho Amaro <gabryel808einstein@gmail.com>
2022-04-20 21:30:57 -0300

    criando exercícios

--- exerícios entrega/calculadora de ic.py
+++ exerícios entrega/calculadora de ic.py
@@ -1,4 +1,24 @@
-peso = int(input("digite em kilos"))
-if peso> 400 and peso<5:
-    print("peso invalido")
-altura = float(input("digite sua altura em metros"))
+while True:
+    altura = float(input("digite sua altura em metros"))
+    if altura > 3 or altura <0.2:
+        print(" altura ivalida digite novamente.")
+    else:
+        break
+
+while True:
+    peso = float(input("digite sua peso em kilos"))
+    if peso > 400 or peso <5:
+        print(" peso ivalido digite novamente.")
+    else:
+        break
+imc = peso / altura**2
+if imc >=18.5 and imc<= 24.9:
+    print("peso normal")
+elif imc >=25 and imc <=29.9:
+    print("Sobrepeso")
+elif imc >30 and imc <40:
+    print("obesidade")
+elif imc>40:
+    print("obesidade grave")
+else:
+    print("abaixo do peso")
