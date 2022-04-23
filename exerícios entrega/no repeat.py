d180c18fb052188017e29fcd992f91d7d973cf12
Gabryel Einstein de Carvalho Amaro <gabryel808einstein@gmail.com>
2022-04-23 09:27:15 -0300

    repetidos

---
+++ exerícios entrega/no repeat.py
@@ -0,0 +1,18 @@
+p1= [1,2,3,8,9]
+p2 = [1,2,3,11,12]
+p3=[]
+p1ToP2  = p1[:]
+
+p1ToP2.extend(p2)
+print(p1ToP2)
+x = 0
+while x < len(p1ToP2):
+    y=0
+    while y <len(p3):
+        if p1ToP2[x]==p1ToP2[y]:
+            break
+        y+=1
+    if y==len(p3):
+        p3.append(p1ToP2[x])
+    x +=1
+print(p3)
