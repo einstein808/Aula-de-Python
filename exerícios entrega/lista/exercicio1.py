94f3ac35859b3983374b5120ace0880ca6a20b29
testando merge <gabryel808einstein@gmail.com>
2022-05-16 21:54:26 -0300

    tttes

--- exerícios entrega/lista/exercicio1.py
+++ exerícios entrega/lista/exercicio1.py
@@ -2,8 +2,12 @@
 n = int(input("digite quantos numeros fatoriais "))
 dados = []
 cont = 0
+final = 1
 while cont < n:
     aux = int(input(f"digite valor {cont + 1}: "))
-    dados.append(aux)
     cont += 1
-print(dados)
+    for x in range(1, aux + 1):
+        final *= x
+    print(final)
+    final = 1
+    x = 1
